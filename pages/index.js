49d3cc10f88ef947ef8e1cca1aa80b79af974346
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2022-06-25 00:22:17 +0530

    last

--- pages/index.js
+++ pages/index.js
@@ -12,21 +12,15 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Click The link below</h1>
+        <h4 className={styles.title}>Click The link below</h4>
         <Link href="/dashboard"> Launch app</Link>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        Powered by{" "}
+        <span className={styles.logo}>
+          <Image src="/logo.png" alt="Vercel Logo" width={12} height={16} />
+        </span>
       </footer>
     </div>
   );
